2a351ff3afcca93071a8bf9ff303bcad8fb2d7b0
nikita.mishchenko <nekitoso2@yandex.ru>
2022-04-05 15:56:38 +0300

    Adding switch-case construction

--- Program.cs
+++ Program.cs
@@ -4,3 +4,23 @@
     либо задать на старте выполнения алгоритма.
     При решении не рекомендуется пользоваться коллекциями,
     лучше обойтись исключительно массивами. */
+
+    
+Console.WriteLine("Program starting...\nChoose option: \n1. Manual entry of strings\n2. Use prepared data\n   Type Q to exit");
+char answerQuestion = char.Parse(Console.ReadLine());
+
+switch (answerQuestion)
+{
+    case '1':
+        Console.WriteLine("First method");
+        break;
+    case '2':
+        Console.WriteLine("Second method");
+        break;
+    case 'Q':
+        Console.WriteLine("Program shutdown");
+        break;
+    case 'q':
+        Console.WriteLine("Program shutdown");
+        break;
+}
